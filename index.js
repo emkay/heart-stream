51f168a18fa242f694765de2f52c9be1588cfbb0
Michael Matuzak <mmatuzak@gmail.com>
2016-02-16 22:22:15 -0800

    :sparkling_heart: fat arrow :sparkling_heart:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const from = require('from2')
 const heart = '💖 '
 
 const heartStream = nes(() => {
-  return from(function (size, next) {
+  return from((size, next) => {
     if (heart.length <= 0) return next(null, null)
     const chunk = heart.slice(0, size) || null
     next(null, chunk)
