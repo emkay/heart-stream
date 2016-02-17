f959f20bc62428f52dc37816b8d12ed4c3d5ae18
Michael Matuzak <mmatuzak@gmail.com>
2016-02-16 22:28:35 -0800

    set correct content-type header for example

--- example.js
+++ example.js
@@ -2,5 +2,6 @@ const http = require('http')
 const hs = require('.')
 
 http.createServer((req, res) => {
+  res.writeHead(200, {'content-type': 'text/html; charset=utf-8'})
   hs.pipe(res)
 }).listen(8080)
